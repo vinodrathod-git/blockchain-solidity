1075e1985ba791de6994c3125aaa1d98aed42a61
vinodrathod-git <62318053+vinodrathod-git@users.noreply.github.com>
2020-04-09 17:17:51 +0530

    calculator with external file

---
+++ solidity-basic/SimpleCalculatorExternalFile.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.5.1;
+
+import "./Calculator.sol";
+//best site to learn library https://medium.com/@jeancvllr/solidity-tutorial-all-about-libraries-762e5a3692f9
+
+contract SimpleCalculator {
+    uint256 public value;
+    
+    function multiple(uint _value1, uint _value2) public {
+        value = Calculator.multiple(_value1, _value2);
+    }
+    
+    function addition(uint _value1, uint _value2) public {
+        value = Calculator.addition(_value1, _value2);
+    }
+    
+    function subraction(uint _value1, uint _value2) public {
+        value = Calculator.subraction(_value1, _value2);
+    }
+    
+    function divide(uint _value1, uint _value2) public {
+        value = Calculator.divide(_value1, _value2);
+    }
+}
